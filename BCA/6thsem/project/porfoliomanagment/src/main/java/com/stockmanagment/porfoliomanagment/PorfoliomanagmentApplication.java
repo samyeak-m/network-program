57c61a87834e54d58dd1b364d0b1327fae1aa226
samyeak-m <maharjansamyak@gmail.com>
2024-05-24 06:46:31 +0545

    new

---
+++ BCA/6thsem/project/porfoliomanagment/src/main/java/com/stockmanagment/porfoliomanagment/PorfoliomanagmentApplication.java
@@ -0,0 +1,13 @@
+package com.stockmanagment.porfoliomanagment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PorfoliomanagmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PorfoliomanagmentApplication.class, args);
+	}
+
+}
