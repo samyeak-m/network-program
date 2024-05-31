eacb32a539bf93bdd15462467c2b4d4b1380eed5
samyeak-m <maharjansamyak@gmail.com>
2024-05-31 09:34:11 +0545

    UPDATE : window event

--- BCA/6thsem/mobileprogramming/app/src/main/java/com/mobile/first/Relative_main.java
+++ BCA/6thsem/mobileprogramming/app/src/main/java/com/mobile/first/Relative_main.java
@@ -1,6 +1,9 @@
 package com.mobile.first;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class Relative_main extends AppCompatActivity {
+    Button btn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +24,15 @@ public class Relative_main extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+
+            btn = findViewById(R.id.button);
+            btn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(Relative_main.this, "Button Clicked", Toast.LENGTH_SHORT).show();
+                }
         });
     }
 }
