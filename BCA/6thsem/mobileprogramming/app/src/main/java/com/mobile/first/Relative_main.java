b9162625bbd99ae416404173dbc314a54e1fc5a2
samyeak-m <maharjansamyak@gmail.com>
2024-06-06 07:28:49 +0545

    UPDATE : activity_relative; create radio group checkbox sipnner(dropdown)

--- BCA/6thsem/mobileprogramming/app/src/main/java/com/mobile/first/Relative_main.java
+++ BCA/6thsem/mobileprogramming/app/src/main/java/com/mobile/first/Relative_main.java
@@ -2,7 +2,10 @@ package com.mobile.first;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -13,6 +16,9 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class Relative_main extends AppCompatActivity {
     Button btn;
+
+    Spinner spinner;
+    String[] spinnerItems ={"c","c++","java","python","kotlin","php","javascript","ruby","swift","objective-c","c#","scala","perl","r","go","html","css","sql","shell","typescript","rust","dart","groovy","lua","matlab","vba","cobol","fortran","abap","pascal","ada","lisp","scheme","prolog","haskell","erlang","ocaml","f#","clojure","kotlin","racket","elm","julia","smalltalk","forth","apex","bash","powershell","pl/sql","t-sql","pl/pgsql","pl/pgsql","mysql","postgresql","sqlite","oracle","mongodb","redis","cassandra","couchbase","couchdb","dynamodb","firebase","realm","sql server","db2","sybase"};
 
 
     @Override
@@ -28,11 +34,28 @@ public class Relative_main extends AppCompatActivity {
 
 
             btn = findViewById(R.id.button);
+            spinner = findViewById(R.id.spinner);
             btn.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     Toast.makeText(Relative_main.this, "Button Clicked", Toast.LENGTH_SHORT).show();
                 }
+        });
+
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, spinnerItems);
+        spinner.setAdapter(arrayAdapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(Relative_main.this, spinnerItems[position], Toast.LENGTH_SHORT).show();
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
         });
     }
 }
