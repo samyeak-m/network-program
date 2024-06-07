1eee439de753e2fc1127eeb82c792199cbe49faf
samyeak-m <maharjansamyak@gmail.com>
2024-06-07 09:22:36 +0545

    CREATE : form with spinner and button toast

--- BCA/6thsem/mobileprogramming/app/src/main/java/com/mobile/first/paletteg.java
+++ BCA/6thsem/mobileprogramming/app/src/main/java/com/mobile/first/paletteg.java
@@ -1,6 +1,12 @@
 package com.mobile.first;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +15,11 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class paletteg extends AppCompatActivity {
+    Button btn;
+    Spinner spinner;
+
+    String[] spinnerItems={"Employee Type","Permanent","Temporary","contract","Freelancer"};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +31,37 @@ public class paletteg extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        btn = findViewById(R.id.submit);
+        spinner = findViewById(R.id.emptype);
+
+        btn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Toast.makeText(paletteg.this,"Submit", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this,android.R.layout.simple_spinner_dropdown_item,spinnerItems);
+        spinner.setAdapter(arrayAdapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String selectedItem=spinnerItems[position];
+                if (selectedItem.equalsIgnoreCase("Employee Type")){
+                    Toast.makeText(paletteg.this,"Cannot select this", Toast.LENGTH_SHORT).show();
+
+                }else{
+                    Toast.makeText(paletteg.this,selectedItem, Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
     }
 }
